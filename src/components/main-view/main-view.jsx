08fae1cd4b2b6b3d7bd5bc9cddf5f1e4c4c960f3
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-09-27 12:51:14 +0100

    MOVIE API INTEGRATION
    Code added to allow for component to access movie data stored in heroku

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
 export class MainView extends React.Component {
@@ -6,14 +7,22 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-            movies: [
-                {_id: 1, Title: 'Inception', Director: 'Christopher Nolan', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath:'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/9c592dee1ac813fcaf8c93557a487557_675ff711-4f45-4a98-95a5-0f97302b2126_480x.progressive.jpg?v=1573618688'},
-                {_id: 2, Title: 'The Shawshank Redemption', Director: 'Frank Darabont', Description: ' Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath:'https://m.media-amazon.com/images/I/71AzwgLT2WL._AC_SY679_.jpg'},
-                {_id: 3, Title: 'Gladiator', Director: 'Ridley Scott', Description: ' A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath:'https://m.media-amazon.com/images/I/71sj8Yt20qL._AC_SY679_.jpg'}
-            ],
+            movies: [],
             selectedMovie: null,
         };
     }
+
+componentDidMount(){
+    axios.get('https://my-movies-souperapp.herokuapp.com/movies')
+    .then(response => {
+        this.setState({
+            movies: response.data
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
 
 setSelectedMovie(newSelectedMovie) {
     this.setState({
@@ -28,7 +37,8 @@ setSelectedMovie(newSelectedMovie) {
         // return <MovieView movie = {selectedMovie}/>;
 
         if (movies.length === 0){
-            return <div className="main-view"> The list is empty!</div>;
+            return <div className="main-view" />; 
+            // The list is empty!</div>;
         } else {
             return (
             <div className = "main-view">
